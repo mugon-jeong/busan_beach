cd542686f643bc0d30ba7c1cfd0cdbd315f851c5
jmg <jmg41490400@gmail.com>
2022-08-08 12:20:04 +0900

    fix devops

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,15 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
-
+import type { NextPage } from 'next';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <div className="img">
-        <Image
-          src='/../public/asset/img/building.png'
-          alt="preparing the site"
-          width={422}
-          height={420}
-        />
+        <Image src="/asset/img/building.png" alt="preparing the site" width={422} height={420} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
