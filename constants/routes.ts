ba77d9c8abdb785f0b1e8a70463627d710b9a669
jmg <jmg41490400@gmail.com>
2022-08-09 15:16:58 +0900

    fix conflict

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,4 +1,4 @@
-import { API_URL_BASE, BEACH_WEATHER_KEY, LIVING_WEATHER_KEY } from '$config';
+import { AIR_QUALITY_KEY, API_URL_BASE, BEACH_WEATHER_KEY, LIVING_WEATHER_KEY, MEDIUM_FORECAST_KEY } from '$config';
 
 export const API_ROUTES = {
   BEACH: {
@@ -9,9 +9,7 @@ export const API_ROUTES = {
       ULTRA: {
         FORECAST: `${API_URL_BASE}/1360000/BeachInfoservice/getUltraSrtFcstBeach?serviceKey=${BEACH_WEATHER_KEY}`,
       },
-      
     },
-    
   },
   LIVING: {
     UV: `${API_URL_BASE}/1360000/LivingWthrIdxServiceV2/getUVIdxV2?serviceKey=${LIVING_WEATHER_KEY}`,
