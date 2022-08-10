334743bb80c079f67f7042d96b0c6becbb699bef
chaud1024 <chaud1024@gmail.com>
2022-08-10 23:42:02 +0900

    index.tsx styledComponent change to emotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 import Image from 'next/image';
 // import { useGetOceanData } from '$queries/useGetOceanData';
 
