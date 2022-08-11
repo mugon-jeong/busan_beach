9f4adc8eb68ab887e39ea0aa0f28cade473f8345
jmg <jmg41490400@gmail.com>
2022-08-12 02:30:57 +0900

    fix production host url

--- config/index.ts
+++ config/index.ts
@@ -15,7 +15,7 @@ interface Config {
 
 const config = getConfig();
 const publicRuntimeConfig: Config = config.publicRuntimeConfig;
-export const HOST_URL = 'http://localhost:3000';
+export const HOST_URL = env === 'local' ? 'http://localhost:3000' : 'https://busanbeachweather.com';
 export const {
   NEXT_PUBLIC_ENV,
   API_URL_BASE,
