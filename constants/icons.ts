83dbc691c4241544412ffec5c14545bda47a3ef5
chaud1024 <chaud1024@gmail.com>
2022-08-11 14:45:13 +0900

    icons added

---
+++ constants/icons.ts
@@ -0,0 +1,35 @@
+export const Icon: {
+  [key in string]: {
+    url: string;
+    alt: string;
+  };
+} = {
+  맑음: {
+    url: '/asset/icon/sun-solid.svg',
+    alt: '맑음',
+  },
+  구름많음: {
+    url: '/asset/icon/cloud-sun-solid.svg',
+    alt: '구름많음',
+  },
+  흐림: {
+    url: '/asset/icon/cloud-solid.svg',
+    alt: '흐림',
+  },
+  비: {
+    url: '/asset/icon/cloud-showers-heavy-solid.svg',
+    alt: '비',
+  },
+  비나눈: {
+    url: '/asset/icon/cloud-rain-snow.svg',
+    alt: '비나눈',
+  },
+  눈: {
+    url: '/asset/icon/snowflake-solid.svg',
+    alt: '눈',
+  },
+  소나기: {
+    url: '/asset/icon/cloud-showers-heavy-solid.svg',
+    alt: '소나기',
+  },
+};
