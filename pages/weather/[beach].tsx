e0203aef69a4ac60c706604b88c7498b88f8768a
cloudwi <cloudwi@naver.com>
2022-08-12 12:09:32 +0900

    손가락 모션 화면 전환 간단
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- pages/weather/[beach].tsx
+++ pages/weather/[beach].tsx
@@ -11,25 +11,40 @@ import LayoutLeft from '$components/Layouts/LayoutLeft';
 import LayoutRight from '$components/Layouts/LayoutRight';
 import { useRouter } from 'next/router';
 import { PARAMS } from '$constants/params';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Beach: NextPage = () => {
+  const [posX, setPosX] = useState(0);
+
   const router = useRouter();
   const { beach } = router.query;
   const pram = PARAMS[beach as string];
   return (
-    <ThemeProvider theme={theme}>
-      <LayoutWrap>
-        <LayoutLeft>
-          <TmplFcst nx={pram.FORECAST.NX} ny={pram.FORECAST.NY} />
-          <TmplWater beachCode={pram.BEACH} />
-        </LayoutLeft>
-        <LayoutRight>
-          <TmplFcstTimely />
-          <TmplFcstDaily />
-          <TmplFcstOther areaNo={pram.AREANO} oceanCode={pram.OCEANCODE} areaIndex={pram.AREAINDEX} />
-        </LayoutRight>
-      </LayoutWrap>
-    </ThemeProvider>
+    <div
+      onTouchStart={e => {
+        setPosX(e.changedTouches[0].pageX);
+      }}
+      onTouchEnd={e => {
+        if (e.changedTouches[0].pageX > posX + window.outerWidth / 5) {
+          router.back();
+        }
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <LayoutWrap>
+          <LayoutLeft>
+            <TmplFcst nx={pram.FORECAST.NX} ny={pram.FORECAST.NY} />
+            <TmplWater beachCode={pram.BEACH} />
+          </LayoutLeft>
+          <LayoutRight>
+            <TmplFcstTimely />
+            <TmplFcstDaily />
+            <TmplFcstOther areaNo={pram.AREANO} oceanCode={pram.OCEANCODE} areaIndex={pram.AREAINDEX} />
+          </LayoutRight>
+        </LayoutWrap>
+      </ThemeProvider>
+    </div>
   );
 };
 
