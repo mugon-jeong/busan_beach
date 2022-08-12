7194b87c86882d4cb1f974f3137e604d6a984bfb
jmg <jmg41490400@gmail.com>
2022-08-12 16:48:50 +0900

    add naver analytics

--- components/Seo/Seo.tsx
+++ components/Seo/Seo.tsx
@@ -9,15 +9,7 @@ const Seo = () => {
     '부산광역시 해수욕장의 날씨,수온,파고 등 해수욕에 도움이 되는 정보를 제공한다.',
   ];
   return (
-    <Head>
-      <title>부산해수욕장날씨</title>
-      <meta name={'type'} property="og:type" content="website" />
-      <meta name={'title'} property="og:title" content={title} />
-      <meta name={'description'} property="og:description" content={description} />
-      <meta name={'url'} property="og:url" content="https://busanbeachweather.com" />
-      <meta name={'author'} property="og:article:author" content="물반사람반" />
-      <meta name="naver-site-verification" content="ee1214d056ffb202a72aed111273b21151cbc728" />
-      <link rel="canonical" href="https://busanbeachweather.com/" />
+    <>
       <Script type="text/javascript" src="//wcs.naver.net/wcslog.js" />
       <Script
         id={'naver_script'}
@@ -31,7 +23,17 @@ const Seo = () => {
                 `,
         }}
       />
-    </Head>
+      <Head>
+        <title>부산해수욕장날씨</title>
+        <meta name={'type'} property="og:type" content="website" />
+        <meta name={'title'} property="og:title" content={title} />
+        <meta name={'description'} property="og:description" content={description} />
+        <meta name={'url'} property="og:url" content="https://busanbeachweather.com" />
+        <meta name={'author'} property="og:article:author" content="물반사람반" />
+        <meta name="naver-site-verification" content="ee1214d056ffb202a72aed111273b21151cbc728" />
+        <link rel="canonical" href="https://busanbeachweather.com/" />
+      </Head>
+    </>
   );
 };
 
