1a02bace4e4c3505a162f7a90950ac1009f9b1c7
jmg <jmg41490400@gmail.com>
2022-08-17 16:10:58 +0900

    add plus icon

--- components/Content/BeachContentInfoOther.tsx
+++ components/Content/BeachContentInfoOther.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import styled from '@emotion/styled';
 import ContentOthers from '$components/Content/ContentOthers';
 import Link from 'next/link';
+import Image from 'next/image';
+import { IconURL } from '$constants/icons';
 
 const TitleCenter = styled.div`
   font-size: 0.9rem;
-  text-align: center;
   margin-top: 0.2em;
+  margin-right: 0.2em;
 `;
-
-const FcstTemp = styled.div`
-  font-size: 1.8em;
-  text-align: right;
-  font-weight: 500;
+const TitleWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
 
 export interface beachContentInfoOther {
@@ -22,99 +23,19 @@ export interface beachContentInfoOther {
 }
 
 const BeachContentInfoOther = ({ info }: { info: beachContentInfoOther }) => {
-  const title = info.title.split(' ');
-
-  if (info.title.length > 8) {
-    if (info.link !== '') {
-      return (
-        <>
-          <Link href={info.link}>
-            <a target="_blank">
-              <ContentOthers iconKey={info.icon} />
-              <div>
-                <TitleCenter>
-                  {title[0]}
-                  <br />
-                  {title[1]}
-                  <br />
-                </TitleCenter>
-              </div>
-            </a>
-          </Link>
-        </>
-      );
-    } else {
-      return (
-        <>
+  return (
+    <>
+      <Link href={info.link}>
+        <a target="_blank">
           <ContentOthers iconKey={info.icon} />
-          <div>
-            <TitleCenter>
-              {title[0]}
-              <br />
-              {title[1]}
-              <br />
-            </TitleCenter>
-          </div>
-        </>
-      );
-    }
-  } else {
-    if (info.link !== '') {
-      return (
-        <Link href={info.link}>
-          <a target="_blank">
-            <ContentOthers iconKey={info.icon} />
-            <div>
-              <TitleCenter>
-                {info.title}
-                <br />
-              </TitleCenter>
-            </div>
-          </a>
-        </Link>
-      );
-    } else {
-      return (
-        <>
-          <ContentOthers iconKey={info.icon} />
-          <div>
-            <TitleCenter>
-              {info.title}
-              <br />
-            </TitleCenter>
-          </div>
-        </>
-      );
-    }
-  }
-  if (info.link !== '') {
-    return (
-      <>
-        <ContentOthers iconKey={info.icon} />
-        <div>
-          <TitleCenter>
-            {info.title}
-            <br />
-            <Link href={info.link}>
-              <a target="_blank">바로가기</a>
-            </Link>
-          </TitleCenter>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <ContentOthers iconKey={info.icon} />
-        <div>
-          <TitleCenter>
-            {info.title}
-            <br />
-          </TitleCenter>
-        </div>
-      </>
-    );
-  }
+          <TitleWrap>
+            <TitleCenter>{info.title}</TitleCenter>
+            <Image src={IconURL['PLUS'].url} alt={IconURL['PLUS'].alt} width={12} height={12} />
+          </TitleWrap>
+        </a>
+      </Link>
+    </>
+  );
 };
 
 export default BeachContentInfoOther;
