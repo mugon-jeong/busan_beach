4e03ec80460ba12e55982f15ae198c78618161ad
chaud1024 <chaud1024@gmail.com>
2022-08-17 11:37:02 +0900

    beach code

--- components/Organisms/OFcstNow.tsx
+++ components/Organisms/OFcstNow.tsx
@@ -7,14 +7,14 @@ import AsyncBoundaryWithQuery from '$components/Boundary/AsyncBoundaryWithQuery'
 import SkeletonWeather from '$components/Molecules/SkeletonWeather';
 import ErrorMesage from '$components/Molecules/ErrorMessage';
 
-const OFcstNow = ({ beach, nx, ny }: { beach: string; nx: number; ny: number }) => {
+const OFcstNow = ({ beach, beachCode }: { beach: string; beachCode: number }) => {
   const [month, date, day] = [getCurrentMonth(), getCurrentDate(), convertCurrentDay()];
   return (
     <>
       <AtomDate today={`${month}월 ${date}일 ${day}요일`} />
       <AtomPlace place={beach} />
-      <AsyncBoundaryWithQuery pendingFallback={<SkeletonWeather />} rejectedFallback={() => <ErrorMesage />}>
-        <Weather nx={nx} ny={ny} />
+      <AsyncBoundaryWithQuery pendingFallback={<SkeletonWeather />}>
+        <Weather beachCode={beachCode} />
       </AsyncBoundaryWithQuery>
     </>
   );
